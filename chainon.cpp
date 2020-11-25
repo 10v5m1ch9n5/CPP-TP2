9f70553dc7f785670f362bf980e776051e1bdc99
Lucas Loiseau <lucas.loiseau@insa-lyon.fr>
2020-11-25 17:10:53 +0100

    Liste chainée avec ajout

--- chainon.cpp
+++ chainon.cpp
@@ -1,8 +1,9 @@
 #include "chainon.h"
 #include <iostream>
+using namespace std;
 
 // Constructeurs
-Chainon::Chainon(TrajetSimple* ts, Chainon* suivant)
+Chainon::Chainon(TrajetSimple* ts = nullptr, Chainon* suivant = nullptr)
 {
 #ifdef MAP
     cout << "Contructeur de Chainon appelé" << endl;
@@ -11,19 +12,37 @@ Chainon::Chainon(TrajetSimple* ts, Chainon* suivant)
     this->ts = ts;
 }
 
-Chainon::Chainon()
-{
-#ifdef MAP
-    cout << "Contructeur de Chainon appelé" << endl;
-#endif
-    this->suivant = nullptr;
-    this->ts = nullptr;
-}
-// Méthodes
+// Get Set
 void Chainon::SetSuivant(Chainon *c) {
     this->suivant = c;
 }
 
 void Chainon::SetTrajetSimple(TrajetSimple *ts) {
     this->ts = ts;
+}
+
+Chainon* Chainon::GetSuivant() {
+    return this->suivant;
+}
+
+TrajetSimple* Chainon::GetTrajetSimple() {
+    return this->ts;
+}
+
+// Méthodes
+void Chainon::ToString() {
+    cout << "Chainon : ";
+    if(this->ts == nullptr)
+    {
+        cout << "vide" << endl;
+        return;
+    }
+    cout << endl;
+    this->ts->ToString();
+    if(this->suivant == nullptr)
+    {
+        cout << "chainon suivant inexistant" << endl;
+        return;
+    }
+    cout << "chainon suivant défini" << endl;
 }
