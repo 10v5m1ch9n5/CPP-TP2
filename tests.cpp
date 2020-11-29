0b51e705569cfac1055c424e0e09c1626fbfc84b
Lucas Loiseau <lucas.loiseau@insa-lyon.fr>
2020-11-29 15:17:51 +0100

    Test de lecture stdin

--- tests.cpp
+++ tests.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+using namespace std;
 #include "liste_chainee.h"
 
 int main()
 {
+    char* ville = new char[25];
+    cin.getline(ville, 25);
+    cout << ville << endl;
+
     TrajetSimple* ts = new TrajetSimple("Paris Gare de Lyon", "Lyon Part-Dieu", "TGV");
     TrajetSimple* ts2 = new TrajetSimple("Lyon Part-Dieu", "Marseille Saint-Charles", "TGV");
     TrajetSimple* ts3 = new TrajetSimple("Lyon Part-Dieu", "Gaston Berger", "Tramway");
