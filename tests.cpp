220d365ab3c68e78149816ad936af6f6b9dc1536
Lucas Loiseau <lucas.loiseau@insa-lyon.fr>
2020-12-16 12:48:21 +0100

    Plus de tests

--- tests.cpp
+++ tests.cpp
@@ -9,11 +9,11 @@ int main()
     TrajetSimple* ts5 = new TrajetSimple("Robinson RER", "Chatelet Les Halles", "RER B");
     TrajetSimple* ts6 = new TrajetSimple("Chatelet Les Halles", "Gare de Lyon", "RER A");
     TrajetSimple* ts7 = new TrajetSimple("Chatelet Les Halles", "Gare de Lyon", "Metro 14");
-    // TrajetSimple* ts = new TrajetSimple("Paris Gare de Lyon", "Lyon Part-Dieu", "TGV");
+    TrajetSimple* ts = new TrajetSimple("Gare de Lyon", "Lyon Part-Dieu", "TGV");
     TrajetSimple* ts9 = new TrajetSimple("Paris CDG", "Tokyo NRT", "Avion");
     TrajetSimple* ts2 = new TrajetSimple("Lyon Part-Dieu", "Marseille Saint-Charles", "TGV");
-    // TrajetSimple* ts3 = new TrajetSimple("Lyon Part-Dieu", "Gaston Berger", "Tramway");
-    // TrajetSimple* ts8 = new TrajetSimple("Paris Gare de Lyon", "Marseille Saint-Charles", "TGV");
+    TrajetSimple* ts3 = new TrajetSimple("Lyon Part-Dieu", "Gaston Berger", "Tramway");
+    // TrajetSimple* ts8 = new TrajetSimple("Gare de Lyon", "Marseille Saint-Charles", "TGV");
     TrajetSimple* ts10 = new TrajetSimple("Tokyo NRT", "Nippori", "Skyliner");
     TrajetSimple* ts11 = new TrajetSimple("Tokyo NRT", "Tokyo", "Narita Express");
     TrajetSimple* ts12 = new TrajetSimple("Nippori", "Ueno", "JR Yamanote");
@@ -30,9 +30,9 @@ int main()
 
     Catalogue catalogue;
 
-    // catalogue.AjouterTrajet(ts);
+    catalogue.AjouterTrajet(ts);
     catalogue.AjouterTrajet(ts2);
-    // catalogue.AjouterTrajet(ts3);
+    catalogue.AjouterTrajet(ts3);
     catalogue.AjouterTrajet(ts5);
     catalogue.AjouterTrajet(ts6);
     catalogue.AjouterTrajet(ts7);
@@ -49,6 +49,10 @@ int main()
 
     cout << CouleurTTY(ROUGE) << "RESULTATS RECHERCHE RECURSIVE" << CouleurTTY(RESET) << endl;
     catalogue.RechercheAvancee("Robinson RER", "Akihabara");
+    cout << CouleurTTY(ROUGE) << "RESULTATS RECHERCHE RECURSIVE" << CouleurTTY(RESET) << endl;
+    catalogue.RechercheAvancee("Robinson RER", "Marseille Saint-Charles");
+    cout << CouleurTTY(ROUGE) << "RESULTATS RECHERCHE RECURSIVE" << CouleurTTY(RESET) << endl;
+    catalogue.RechercheAvancee("Chatelet Les Halles", "Marseille Saint-Charles");
 
     return 0;
 }
