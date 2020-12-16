61c4b2676b0264d2da5959bb28cff93f9ac5cf60
Lucas Loiseau <lucas.loiseau@insa-lyon.fr>
2020-12-16 19:22:38 +0100

    Constructeur de copie

--- trajet_simple.h
+++ trajet_simple.h
@@ -14,6 +14,7 @@ class TrajetSimple : public Trajet
 // Constructeurs
     public:
     TrajetSimple(const char* villeDepart, const char* villeArrivee, const char* moyenTransport);
+    TrajetSimple(const TrajetSimple & t);
 // Méthodes
     void ToString() override;
     const char* GetDepart() override;
